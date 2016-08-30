c3f04a052583cacc18ec3c60cb02d0041106288b
lufficc <luffy.lcc@gmail.com>
2016-08-31 02:41:57 +0800

    test data

--- app/src/main/java/com/lufficc/demolightadapter/MainActivity.java
+++ app/src/main/java/com/lufficc/demolightadapter/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
+import com.lufficc.demolightadapter.model.ImgModel;
+import com.lufficc.demolightadapter.model.TextModel;
+import com.lufficc.demolightadapter.viewprovider.ImgViewProvider;
+import com.lufficc.demolightadapter.viewprovider.TextViewProvider;
 import com.lufficc.lightadapter.LightAdapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+
+        adapter.register(TextModel.class,new TextViewProvider());
+        adapter.register(ImgModel.class,new ImgViewProvider());
+
     }
 }
